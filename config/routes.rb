b5bb25732fc2a06adddd3c9bbf195f6b1b900c15
Koji Yagyu <1811140080p@ed.fuk.kindai.ac.jp>
2020-11-18 05:31:33 +0000

    controller model作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'evals/new'
+  get 'evals/edit'
+  get 'stores/new'
+  get 'stores/edit'
+  get 'users/new'
+  get 'users/edit'
+  get 'top/index'
+  get 'top/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
