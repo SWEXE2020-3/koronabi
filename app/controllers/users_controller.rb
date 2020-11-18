94ddfd2591eee8bf6ef39c99f76b82f4c620036d
Koji Yagyu <1811140080p@ed.fuk.kindai.ac.jp>
2020-11-18 05:54:52 +0000

    edit user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(uname: params[:uname], pass: params[:pass])
+    if @user.save
+    end
   end
 
   def edit
