76ec5e1bcdad1fcf66b07ee58d077978b153402d
Koji Yagyu <1811140080p@ed.fuk.kindai.ac.jp>
2020-11-18 05:59:57 +0000

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'top/index'
   get 'top/login'
+  resources :evals
+  resources :stores
+  resources :users
+  resources :top
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
