9ccab7cbd87c83af0063c160d2df408ac4bf5e24
Tomoki Iwanaga <1811140064f@ed.fuk.kindai.ac.jp>
2020-12-02 06:17:03 +0000

    お店登録ページ編集

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -8,7 +8,8 @@ class StoresController < ApplicationController
   end
   
   def create
-    store = Store.new(sname: params[:store][:sname], addr: params[:store][:addr], tel: params[:store][:tel], open: params[:store][:open])
+    store = Store.new(sname: params[:store][:sname], addr: params[:store][:addr], tel: params[:store][:tel], open: params[:store][:open],
+                      eval1: params[:store][:eval1], eval2: params[:store][:eval2], eval3: params[:store][:eval3], eval4: params[:store][:eval4], eval5: params[:store][:eval5])
     store.save
     redirect_to top_index_path
   end
@@ -25,7 +26,8 @@ class StoresController < ApplicationController
   
   def update
     store = Store.find(params[:id])
-    store.update(sname: params[:store][:sname], addr: params[:store][:addr], tel: params[:store][:tel], open: params[:store][:open])
+    store.update(sname: params[:store][:sname], addr: params[:store][:addr], tel: params[:store][:tel], open: params[:store][:open],
+                 eval1: params[:store][:eval1], eval2: params[:store][:eval2], eval3: params[:store][:eval3], eval4: params[:store][:eval4], eval5: params[:store][:eval5])
     redirect_to top_index_path
   end
 end
